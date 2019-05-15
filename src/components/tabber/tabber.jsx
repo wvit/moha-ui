528a167546045c9017dfbf07ad1c6dd04f2f824e
wvit <1999wuwei@gmail.com>
2019-05-15 18:56:08 +0800

    修改tabber配置

--- src/components/tabber/tabber.jsx
+++ src/components/tabber/tabber.jsx
@@ -1,34 +1,39 @@
 import React from 'react';
 import { BrowserRouter, NavLink, Switch, Route, Redirect } from 'react-router-dom';
+import util from '../../assets/js/util.js';
 import '../../assets/css/tabber.css';
 
 class Tabber extends React.Component {
   render() {
-    const { tabberConfig, activeClassName, className } = this.props;
+    const { tabberConfig: { iconType, selectedColor, list } } = this.props;
     return (
       <BrowserRouter>
         <Switch>
           <Route exact path="/" render={() =>
-            <Redirect to={tabberConfig[0].path}></Redirect>}>
+            <Redirect to={list[0].path}></Redirect>}>
           </Route>
-          {tabberConfig.map((item, index) => {
+          {list.map((item, index) => {
             return <Route key={index} path={item.path} component={item.component} />
           })}
         </Switch>
         <div className="tabber-wrap">
+          <div className="tabber-padding-box"></div>
           <ul className="clearfix">
             {
-              tabberConfig.map((item, index) => {
+              list.map((item, index) => {
                 return <li
                   key={index}
                   style={{
-                    width: `${100 / tabberConfig.length}%`
+                    width: `${100 / list.length}%`
                   }}
                 >
                   <NavLink
-                    activeClassName={`active ${activeClassName}`}
-                    className={`tabber-link ${className}`}
+                    className='tabber-link'
+                    activeClassName='active'
+                    style={{ background: selectedColor, lineHeight: iconType ? '16px' : '30px' }}
+                    activeStyle={{ background: '#fff', color: selectedColor }}
                     to={item.path}>
+                    {this.setTabberIcon(iconType, item)}
                     {item.name}
                   </NavLink>
                 </li>
@@ -38,6 +43,27 @@ class Tabber extends React.Component {
         </div>
       </BrowserRouter>
     )
+  }
+  //监听dom挂载完毕
+  componentDidMount() {
+    const box = util.query('.tabber-padding-box')[0];
+    const wrap = util.query('.tabber-wrap ul')[0];
+    box.style.height = wrap.offsetHeight + 'px';
+  }
+  //设置tabber icon
+  setTabberIcon(iconType, item) {
+    if (iconType === 'iconfont') {
+      return (
+        <span className={`tabber-iconfont ${item.className}`}></span>
+      )
+    } else if (iconType === 'image') {
+      return (
+        <span>
+          <img className='tabber-icon' src={item.icon}></img>
+          <img className='tabber-active-icon' src={item.activeIcon}></img>
+        </span>
+      )
+    }
   }
 }
 
--- src/components/title/title.jsx
+++ src/components/title/title.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createBrowserHistory } from 'history'
+import util from '../../assets/js/util.js'
 import '../../assets/css/title.css'
 
 class Title extends React.Component {
@@ -7,13 +8,22 @@ class Title extends React.Component {
     const { titleName, className } = this.props;
     const history = createBrowserHistory();
     return (
-      <div className={`head-title ${className}`} >
-        <span className="head-back" onClick={() => { history.goBack() }}></span>
-        <p className="head-text">
-          {titleName}
-        </p>
+      <div className="head-wrap">
+        <div className="head-padding-box"></div>
+        <div className={`head-title ${className}`} >
+          <span className="head-back" onClick={() => { history.goBack() }}></span>
+          <p className="head-text">
+            {titleName}
+          </p>
+        </div>
       </div>
     )
+  }
+  //监听dom挂载完毕
+  componentDidMount() {
+    const box = util.query('.head-padding-box')[0];
+    const head = util.query('.head-title')[0];
+    box.style.height = head.offsetHeight + 'px';
   }
 }
 
